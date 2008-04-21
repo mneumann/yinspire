3bf93ab0540eddd3ba99d0da84ff6af0826b4fdf
Michael Neumann <mneumann@ntecs.de>
2008-04-21 16:29:02 +0200

    Make it usable as a Ruby library, not only from the command line.

--- tools/Compare_EventLists.rb
+++ tools/Compare_EventLists.rb
@@ -1,50 +1,58 @@
-def parse(filename)
-  a = {} 
-  File.readlines(filename).each do |line|
-    id, time = line.split("!", 2)
-    next if time.nil?
-    (a[id.strip] ||= []) << time.to_f
-  end
-  a.each_value {|arr| arr.sort!}
-  return a
-end
+class Compare_EventLists
+  def compare(fa, fb)
+    res = {}
+    a = parse(fa)
+    b = parse(fb)
 
-def maximum(a)
-  a.map {|k,v| v.max}.max
-end
+    max = [maximum(a), maximum(b)].max
 
-a = parse(ARGV.shift)
-b = parse(ARGV.shift)
+    as = a.inject(0) {|s, kv| s + kv[1].size} 
+    bs = b.inject(0) {|s, kv| s + kv[1].size} 
+    max_sz = [as, bs].max
 
-max = [maximum(a), maximum(b)].max
+    raise "keys failed" if a.keys.sort != b.keys.sort
+    res[:total_size_diff_percentage] = ((as - bs).abs * 100.0 / max_sz)
 
-as = a.inject(0) {|s, kv| s + kv[1].size} 
-bs = b.inject(0) {|s, kv| s + kv[1].size} 
-max_sz = [as, bs].max
+    grand_total_err = 0.0
 
-STDERR.puts "Total size differs: #{((as - bs).abs * 100.0 / max_sz)} %"
-raise "keys failed" if a.keys.sort != b.keys.sort
+    for k in a.keys
+      err = 0.0  
+      n = [a[k].size, b[k].size].min
 
-grand_total_err = 0.0
+      for i in 0 ... n 
+        err += (a[k][i] - b[k][i]).abs
+      end
 
-for k in a.keys
+      total_err = err / n
+      grand_total_err += total_err
+    end
 
-  diff_sz = (a[k].size - b[k].size).abs
-  if diff_sz > 3
-    STDERR.puts "number of fires differs too much for key #{k} (#{diff_sz})"
-  end
+    grand_total_err /= a.keys.size
 
-  err = 0.0  
-  n = [a[k].size, b[k].size].min
+    res[:grand_total_error] = grand_total_err
 
-  for i in 0 ... n 
-    err += (a[k][i] - b[k][i]).abs
+    res
   end
 
-  total_err = err / n
-  grand_total_err += total_err
+  private
+
+  def parse(filename)
+    a = {} 
+    File.readlines(filename).each do |line|
+      id, time = line.split("!", 2)
+      next if time.nil?
+      (a[id.strip] ||= []) << time.to_f
+    end
+    a.each_value {|arr| arr.sort!}
+    return a
+  end
+
+  def maximum(a)
+    a.map {|k,v| v.max}.max
+  end
 end
 
-grand_total_err /= a.keys.size
-
-STDERR.puts "grand total error: #{grand_total_err}"
+if __FILE__ == $0
+  require 'pp'
+  pp Compare_EventLists.new.compare(*ARGV)
+end
