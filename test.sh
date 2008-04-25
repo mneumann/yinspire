d7322c106a486ee70b3da10b999db8a2f5fadbe1
Michael Neumann <mneumann@ntecs.de>
2008-04-25 19:29:18 +0200

    *

--- test.sh
+++ test.sh
@@ -1,17 +1,15 @@
 STOP_AT=2000
 
 mkdir test/
-for i in Default Debug Release; do
+for i in Debug Release; do
   time build/$i/yinspire --stop-at ${STOP_AT} --record test/$i $@
 done 
 
-for i in Default Debug Release; do
+for i in Debug Release; do
   time wine build/$i.Win32/yinspire.exe --stop-at ${STOP_AT} --record test/$i.Win32 $@
 done
 
 echo "OK"
-ruby tools/Compare_EventLists.rb test/Default test/Debug 
 ruby tools/Compare_EventLists.rb test/Debug test/Release 
-ruby tools/Compare_EventLists.rb test/Release test/Default.Win32
-ruby tools/Compare_EventLists.rb test/Default.Win32 test/Debug.Win32
+ruby tools/Compare_EventLists.rb test/Release test/Debug.Win32
 ruby tools/Compare_EventLists.rb test/Debug.Win32 test/Release.Win32 
