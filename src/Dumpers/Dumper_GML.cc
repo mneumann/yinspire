c44def94c8eebe39ac7e497c5816192204748d7d
Michael Neumann <mneumann@ntecs.de>
2016-01-18 22:01:16 +0100

    Use integer for GML id instead of string.
    gml2gv, which converts GML to graphviz dot requires this.

--- src/Dumpers/Dumper_GML.cc
+++ src/Dumpers/Dumper_GML.cc
@@ -5,37 +5,54 @@
 #include "Core/Properties.h"
 #include "Dumpers/Dumper_GML.h"
 #include "Models/Synapse_Base.h"
+#include <string>
+#include <map>
 
 namespace Yinspire {
 
+  struct Context {
+      FILE *fh;
+      std::map<string, int> mapping;
+      int next_id;
+  };
+
   static void dump_node(NeuralEntity *entity, void *data)
   {
-    FILE *fh = (FILE*) data;
+    Context *c = (Context*) data;
+    FILE *fh = c->fh;
 
     /*
      * Reject Synapses
      */ 
     if (dynamic_cast<Neuron*>(entity) == NULL) return;
 
+    int my_id = c->next_id++;
+    c->mapping[entity->get_id()] = my_id;
+
     fprintf(fh, "  node\n");
     fprintf(fh, "  [\n");
-    fprintf(fh, "    id %s\n", entity->get_id().c_str());
+    fprintf(fh, "    id %d\n", my_id);
+    fprintf(fh, "    label \"%s\"\n", entity->get_id().c_str());
     fprintf(fh, "  ]\n");
   }
 
   static void dump_edge(NeuralEntity *entity, void *data)
   {
-    FILE *fh = (FILE*) data;
+    Context *c = (Context*) data;
+    FILE *fh = c->fh;
 
     /*
      * Reject Neurons
      */ 
     const Synapse *syn = dynamic_cast<const Synapse*>(entity);
     if (syn) {
+        int source_id = c->mapping[syn->get_pre_neuron()->get_id()];
+        int target_id = c->mapping[syn->get_post_neuron()->get_id()];
+
         fprintf(fh, "  edge\n");
         fprintf(fh, "  [\n");
-        fprintf(fh, "    source %s\n", syn->get_pre_neuron()->get_id().c_str());
-        fprintf(fh, "    target %s\n", syn->get_post_neuron()->get_id().c_str());
+        fprintf(fh, "    source %d\n", source_id);
+        fprintf(fh, "    target %d\n", target_id);
 
         const Synapse_Base *syn_base = dynamic_cast<const Synapse_Base*>(syn);
         if (syn_base) {
@@ -49,12 +66,16 @@ namespace Yinspire {
 
   void Dumper_GML::dump(FILE *fh)
   {
+    Context c;
+    c.fh = fh;
+    c.next_id = 1;
+
     fprintf(fh, "graph\n");
     fprintf(fh, "[\n");
     fprintf(fh, "  directed 1\n");
 
-    net->each(dump_node, fh);
-    net->each(dump_edge, fh);
+    net->each(dump_node, &c);
+    net->each(dump_edge, &c);
     fprintf(fh, "]\n");
   }
 
